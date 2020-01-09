9bfe264494b88ef7308ef8cbf7b750d88d3ae130
Mr. Jonah <jaitch@gmail.com>
2020-01-09 12:58:30 -0600

    Agent Neo, added

--- src/components/AgentLoggedIn/AgentTrips.js
+++ src/components/AgentLoggedIn/AgentTrips.js
@@ -10,26 +10,31 @@ const AgentTrips = (props) => {
     const [tripList, setTripList] = useState({});
 
     useEffect(() => {
-        axiosWithAuth().get(`/trip/agent`)
-        .then(res => setTripList(res.data))
-        .catch(err => console.log(err.response))
+        axiosWithAuth().get(`/trip/agent/${currentUser.id}`)
+            .then(res => setTripList(res.data))
+            .catch(err => console.log(err.response))
     }, [])
 
     return (
         <Container>
             <Row>
                 <Col xs="12" className="text-center">
-                    <h3 className="mt-5 mb-5">Welcome, Agent Smith</h3>
+                    <h3 className="mt-5 mb-5">Welcome, {currentUser.fullname}</h3>
                     <div className="mt-5 mb-5">&nbsp;</div>
                 </Col>
             </Row>
             <Row>
-                <Col xs="12" sm={{size: 8, offset: 2}}>
-                    {/* { tripList.length &&
+                <Col xs="12" sm={{size: 8, offset: 2}} className="mb-5">
+                    { tripList.length &&
                         tripList.map(trip => 
                             <Link key={trip.id} to={'/Agent-Trip-Details/'+trip.id} className="btn btn-outline-dark btn-block">{trip.date} {trip.flight}</Link>
                         )
-                    } */}
+                    }
+                </Col>
+            </Row>
+            <Row>
+                <Col xs="12" sm={{size: 8, offset: 2}}>
+                    <Link to="/Agents" className="btn btn-outline-dark btn-block">Main Menu</Link>
                 </Col>
             </Row>
         </Container>
