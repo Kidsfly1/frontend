3b5bda72fe3966b0e1e26882cbe5f72a99de7252
Chad.Snyder <Mapletonkid@gmail.com>
2020-01-09 11:55:55 -0700

    Add User trip list Broken

--- src/components/FamilyTrips.js
+++ src/components/FamilyTrips.js
@@ -13,9 +13,8 @@ const FamilyHome = () => {
                 <Col xs="12" className="text-center">
                     <h2 className="mt-5 mb-5">Trips</h2>
                     <div className="mt-5 mb-5">&nbsp;</div>
-                    <Link to="/Create-Trip" className="btn btn-block btn-outline-dark mt-5 mb-1 p-4"> Curent Trip</Link>
-                    <Link to="" className="btn btn-block btn-outline-dark p-4"> Upcoming Trips</Link>
-                    <Link to="" className="btn btn-block btn-outline-dark p-4"> Past Trips</Link>
+                    <Link to="/Welcome" className="btn btn-block btn-outline-dark mt-5 mb-1 p-4"> Back to Dashboard</Link>
+                    <Link to="" className="btn btn-block btn-outline-dark p-4"> My Trips</Link>
                 </Col>
             </Row>
         </Container>
