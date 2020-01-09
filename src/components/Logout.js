ced970d482ea4b6347076224729a5de73ee8e2e8
Mr. Jonah <jaitch@gmail.com>
2020-01-09 11:46:34 -0600

    Logout route fixed again... now using logout endpoint

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useContext } from 'react';
 
 import { KidsFlyContext } from '../context/KidsFlyContext';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const Logout = (props) => {
     const {setCurrentUser} = useContext(KidsFlyContext);
 
     useEffect(() => {
-        setCurrentUser({})
+        axiosWithAuth().get('/user/logout')
+            .then(res => setCurrentUser({}))
+            .catch(err => console.log(err))
     }, []);
 
     //  Create as function in App.js and pass down as props
